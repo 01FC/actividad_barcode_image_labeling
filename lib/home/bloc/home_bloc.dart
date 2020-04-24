bc7e5c59f692dfe0659257c1a7fd1670917d54cd
01FC <javier.francisco01.09@gmail.com>
2020-04-23 21:52:49 -0500

    feat: agregado home bloc con logica para lectura de codigos y labeling

--- lib/home/bloc/home_bloc.dart
+++ lib/home/bloc/home_bloc.dart
@@ -26,14 +26,63 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
         String data = "";
         if (event.barcodeScan) {
           // barcode scanner
-
+          data = await _barcodeScan(img);
         } else {
           // image labeling
-
+          data = await _imgLabeling(img);
         }
         yield Results(result: data, chosenImage: img);
       }
     }
+  }
+
+  Future<String> _barcodeScan(File imageFile) async {
+    var visionImage = FirebaseVisionImage.fromFile(imageFile);
+    var barcodeDetector = FirebaseVision.instance.barcodeDetector();
+    List<Barcode> codes = await barcodeDetector.detectInImage(visionImage);
+
+    String data = "";
+    for (var item in codes) {
+      var code = item.rawValue;
+      var type = item.valueType;
+      var boundBx = item.boundingBox;
+      var corners = item.cornerPoints;
+      var url = item.url;
+
+      data += ''' 
+      > Codigo: $code\n
+      > Formato: $type\n
+      > URL titulo: ${url == null ? "No disponible" : url.title}\n
+      > URL: ${url == null ? "No disponible" : url.url}\n
+      > Area de cod: $boundBx\n
+      > Esquinas: $corners\n
+      --------------------\n
+      ''';
+    }
+
+    return data;
+  }
+
+  Future<String> _imgLabeling(File imageFile) async {
+    var visionImage = FirebaseVisionImage.fromFile(imageFile);
+    var labelDetector = FirebaseVision.instance.imageLabeler();
+    List<ImageLabel> labels = await labelDetector.processImage(visionImage);
+
+    String data = "";
+    for (var item in labels) {
+      String id = item.entityId;
+      String label = item.text;
+      double prob = item.confidence;
+
+      data += ''' 
+      > Id: $id\n
+      > Label: $label\n
+      > Certeza: $prob\n
+      --------------------\n
+      ''';
+    }
+
+    return data;
   }
 
   Future<File> _chooseImage() async {
